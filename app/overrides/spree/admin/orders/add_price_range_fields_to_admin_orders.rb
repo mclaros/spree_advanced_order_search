6d4cee7223214cd52e42b7d006b87a8c51d43227
Mainor <m_claros@live.com>
2014-03-11 20:33:44 -0700

    add overrides

---
+++ app/overrides/spree/admin/orders/add_price_range_fields_to_admin_orders.rb
@@ -0,0 +1,17 @@
+Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
+	:name => "add_shipment_status_to_admin_orders",
+	:insert_after => "div.date-range-filter",
+	:text => "
+		<div class='field' id='q_shipment_state'>
+			<label>
+				Shipment Status
+				<br>
+				<select class='select2' id='ship_state' name='q[shipment_state]'>
+					<option value></option>
+					<option value='ready'>Ready</option>
+					<option value='pending'>Pending</option>
+					<option value='shipped'>Shipped</option>
+				</select>
+			</label>
+		</div>
+	")
