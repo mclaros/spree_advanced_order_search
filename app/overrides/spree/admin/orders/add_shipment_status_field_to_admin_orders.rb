8c380a2caa7a5e1cd6a4e2188bb2ca3e6905c57e
Mainor <m_claros@live.com>
2014-03-11 20:49:56 -0700

    fix typos in override

--- app/overrides/spree/admin/orders/add_shipment_status_field_to_admin_orders.rb
+++ app/overrides/spree/admin/orders/add_shipment_status_field_to_admin_orders.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
-	:name => "add_shipment_status_to_admin_orders",
+	:name => "add_shipment_status_field_to_admin_orders",
 	:insert_after => "div.date-range-filter",
 	:text => "
 		<div class='field' id='q_shipment_state'>
