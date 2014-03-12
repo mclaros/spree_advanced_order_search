2baf08459494b21a8853371ad62ba96934c16cb6
Mainor <m_claros@live.com>
2014-03-11 21:36:11 -0700

    remove floating bracket after shipment status field

--- app/overrides/spree/admin/orders/add_shipment_status_field_to_admin_orders.rb
+++ app/overrides/spree/admin/orders/add_shipment_status_field_to_admin_orders.rb
@@ -4,6 +4,6 @@ Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
 	:text => "
 		<div class='field'>
 			<%= f.label :ship_state, 'Shipment Status' %>
-			<%= f.select :shipment_state_eq, ['', 'ready', 'pending', 'shipped'], {:include_blank => true}, :class => 'select2' %>}
+			<%= f.select :shipment_state_eq, ['', 'ready', 'pending', 'shipped'], {:include_blank => true}, :class => 'select2' %>
 		</div>
 		")
